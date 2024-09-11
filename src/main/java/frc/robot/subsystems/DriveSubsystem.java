ee42bf26d45a2cad9eddbf1f98d17f0ac44b26ab
Kaden Chow <kaden.chow@gmail.com>
2024-09-11 16:22:20 -0700

    git ignore

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -10,7 +10,7 @@ import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 
-import edu.wpi.first.wpilibj.XboxController;
+//import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
